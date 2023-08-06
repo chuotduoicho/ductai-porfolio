25cece0c5338029285580d582ec1c5fe29440371
taivd-vbee <taivd@vbee.vn>
2023-08-06 13:34:43 +0700

    Add google analysist

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,32 @@
 import { BrowserRouter } from "react-router-dom";
 
 import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas } from "./components";
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics,logEvent  } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCwPJTBMb9syn3lxzl1C0ho8s0v2TDI1vM",
+  authDomain: "ductai-porfolio.firebaseapp.com",
+  projectId: "ductai-porfolio",
+  storageBucket: "ductai-porfolio.appspot.com",
+  messagingSenderId: "287151649595",
+  appId: "1:287151649595:web:4fec14e993635daff4fd1e",
+  measurementId: "G-6VSGGHJNKV",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 const App = () => {
+
+  logEvent(analytics, 'notification_received');
+
   return (
     <BrowserRouter>
       <div className='relative z-0 bg-primary'>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,28 +5,6 @@ import { styles } from "../styles";
 import { navLinks } from "../constants";
 import { logo, menu, close } from "../assets";
 
-// // Import the functions you need from the SDKs you need
-// import { initializeApp } from "firebase/app";
-// import { getAnalytics, logEvent } from "firebase/analytics";
-// // TODO: Add SDKs for Firebase products that you want to use
-// // https://firebase.google.com/docs/web/setup#available-libraries
-
-// // Your web app's Firebase configuration
-// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-// const firebaseConfig = {
-//   apiKey: "AIzaSyCwPJTBMb9syn3lxzl1C0ho8s0v2TDI1vM",
-//   authDomain: "ductai-porfolio.firebaseapp.com",
-//   projectId: "ductai-porfolio",
-//   storageBucket: "ductai-porfolio.appspot.com",
-//   messagingSenderId: "287151649595",
-//   appId: "1:287151649595:web:4fec14e993635daff4fd1e",
-//   measurementId: "G-6VSGGHJNKV",
-// };
-
-// // Initialize Firebase
-// const app = initializeApp(firebaseConfig);
-// const analytics = getAnalytics(app);
-
 const Navbar = () => {
   const [active, setActive] = useState("");
   const [toggle, setToggle] = useState(false);
